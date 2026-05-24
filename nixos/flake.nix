ba3681b0c72b15cd7c85d800cd46d5f6c574acd9
Lucas <lucas1.eeckhoutte@epitech.eu>
2026-05-24 22:23:59 +0200

    feat!: add all nixos config file

---
+++ nixos/flake.nix
@@ -0,0 +1,38 @@
+{
+    description = "NixOS config - Laptop";
+
+    inputs = {
+        nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+        home-manager = {
+            url = "github:nix-community/home-manager";
+            inputs.nixpkgs.follows = "nixpkgs";
+        };
+        zen-browser = {
+            url = "github:0xc000022070/zen-browser-flake";
+            inputs.nixpkgs.follows = "nixpkgs";
+        };
+	caelestia-shell = {
+	    url = "github:caelestia-dots/shell";
+            inputs.nixpkgs.follows = "nixpkgs";
+        };
+    };
+
+    outputs = inputs @ { self, nixpkgs, home-manager, zen-browser, caelestia-shell, ... }:
+    let
+        system = "x86_64-linux";
+    in {
+        nixosConfigurations.nixos = nixpkgs.lib.nixosSystem {
+            inherit system;
+            specialArgs = { inherit self inputs; };
+            modules = [
+                ./config.nix
+                home-manager.nixosModules.home-manager {
+                    home-manager.useGlobalPkgs = true;
+                    home-manager.useUserPackages = true;
+                    home-manager.users.lucas = import ./home.nix;
+                    home-manager.extraSpecialArgs = { inherit zen-browser system inputs; };
+                }
+            ];
+        };
+    };
+}
